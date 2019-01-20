ca22dec68ed2a90573091769457a26361b8cf077
tdudkowski <tdudkowski@gmail.com>
2019-01-20 03:24:08 +0100

    strong impression that dot problem is solved

--- main.js
+++ main.js
@@ -36,8 +36,9 @@
       let result = parseFloat(finalArr[actionPoint - 1]) - parseFloat(finalArr[actionPoint + 1]);
       finalArr.splice(actionPoint - 1, 3, result);
     }
+    finalArr = Number(finalArr);
     console.log('wynik: ' + finalArr);
-    resultField.textContent = finalArr;
+    resultField.textContent = parseFloat(finalArr.toFixed(5));
     flag = true;
   }
 
@@ -72,15 +73,10 @@
     handleDivision(finalArr);
   }
 
-  // HANDLE_DOTS USUWA NADMIERNE KROPKI I OSTATNI CIĄG JEŚLI TO OPERATOR
+  // HANDLE_DOTS - CZY TO JEST NADAL POTRZEBNE?
   const handleDots = function (finalArr) {
 
-    // usuwa kropki
     for (let i = 0; i < finalArr.length; i++) { // dlaczego tu odliczałem co 2? przecież na 0 może być operator
-      // 1. kropki z początku
-      while ((finalArr[i])[0] === ".") { // ucina kropki z przodu
-        finalArr[i] = finalArr[i].substr(1);
-      }
 
       // usuwa NaN - działa?
       if (typeof finalArr[i] === 'number' && isNaN(finalArr[i])) {
@@ -101,10 +97,8 @@
         finalArr[1] = -(finalArr[1]); // pierwsza liczba staje się ujemna jeżeli przed nią jest minus
       }
       for (let i = 0; i < finalArr.length; i += 2) { // parzysta liczba stringów
-        finalArr[i + 1] = parseFloat(finalArr[i + 1]); // zamiana co drugiego stringu na liczbę
-        if (finalArr[i].length > 1) {
-          finalArr[i] = finalArr[i].substr(-2); // obcięcie do dwóch ostatnich znaków jeżeli jest ich więcej
-          // console.log('elo' + finalArr[i], finalArr[i].length)
+        console.log(finalArr[i])
+        if (finalArr[i].length > 1) { // jeżeli jest więcej niż jeden operator
           if ((finalArr[i])[1] === '-') { // jeśli drugi ze znaków to minus
             finalArr[i + 1] = finalArr[i + 1] * -1; // następna liczba staje się ujemna
             finalArr[i] = finalArr[i].slice(0, 1); // wybrany zostaje pierwszy znak
@@ -113,20 +107,18 @@
           }
         }
       }
-      finalArr.shift();
+      finalArr.shift(); // usunięcie niepotrzebnego pierwszego stringu
     } else { // pierwszy string to nie operator
       console.log('na poczatku nie operator');
       for (let i = 0; i < finalArr.length; i += 2) { // zawsze nieparzysta liczba elementów
-        finalArr[i] = parseFloat(finalArr[i]);
-        if (i < finalArr.length - 1) {
-          finalArr[i + 1] = finalArr[i + 1].substr(-2); // obcięcie ostatnich dwóch znaków z operatorów
-        }
-        if (i < finalArr.length - 1 && (finalArr[i + 1])[1] === '-') {
-          finalArr[i + 2] = -(finalArr[i + 2]);
-          finalArr[i + 1] = finalArr[i + 1].slice(-1);
-        }
-        if (i < finalArr.length - 1) {
-          finalArr[i + 1] = finalArr[i + 1].slice(-1);
+        console.log(finalArr[i])
+        if (i < finalArr.length - 1 && finalArr[i + 1].length > 1) { // jeżeli jest więcej niż jeden operator
+          if ((finalArr[i + 1])[1] === '-') { // jeśli drugi znak poprzedzającego stringu to minus
+            finalArr[i + 2] = -(finalArr[i + 2]); // następna liczba staje się ujemna
+            finalArr[i + 1] = finalArr[i + 1].slice(0, 1);
+          } else {
+            finalArr[i + 1] = finalArr[i + 1].slice(1); // w obu było (-1) dlaczego?
+          }
         }
       }
     }
@@ -140,60 +132,78 @@
 
   // MAKE_A_COUNT UTWORZENIE TABLICY ZE STRINGAMI LICZBA/OPERATOR
   const makeACount = function (toCountArray) {
-    let finalArr = []; // tablica
+    finalArr = []; // tablica
     while (toCountArray.includes('x')) {
-      let terminus = toCountArray.indexOf('x');
-      let nextEl = toCountArray.splice(0, terminus);
-      toCountArray.splice(0, 1);
-      nextEl = nextEl.toString();
+      let terminus = toCountArray.indexOf('x'); // znalezienie pozycji najbliższego ixa
+      let nextEl = toCountArray.splice(0, terminus); // odcięcie od zera do terminusa
+      toCountArray.splice(0, 1); // usunięcie ixa
+      nextEl = nextEl.toString(); // zamiana znakow na string
       nextEl = nextEl.replace(/,/g, ''); // usunięcie przecinków
       finalArr.push(nextEl); // umieszczenie kolejnych stringów w tablicy
     }
-    if (finalArr.length == 1) return;
 
-    // usuwa ostatni string jeśli jest to operator
-    let test2 = (finalArr[finalArr.length - 1])[0];
-    if (operators.includes(test2)) { // czy ostatni string to operator
-      finalArr.pop(); // jeżeli to operator to usuwa go
+    // usuwa ostatni string jeśli jest to operator lub sama tylko kropka
+    let test2 = (finalArr[finalArr.length - 1]);
+    if (operators.includes((test2)[0]) || test2.length == 1 && test2 == ".") { // czy ostatni string to operator lub kropka
+      finalArr.pop(); // jeżeli tak to usuwa go
     }
-    // usuwa string operatórow jeżeli następny string to też operator
 
-    console.log(finalArr);
-    for (let i = 0; i < finalArr.length - 1; i++) {
+    for (let i = 0; i < finalArr.length; i++) {
+      // usuwa string operatórow jeżeli następny string to też operator - czy to jest potrzebne?
       if (operators.includes((finalArr[i])[0]) && operators.includes((finalArr[i + 1])[0])) {
         console.log('usunalem string operatorow ' + i + finalArr[i] + finalArr[i + 1])
         finalArr.splice(i, 1);
       }
+      // ucina kropki z przodu liczby
+      while ((finalArr[i])[0] == ".") {
+        finalArr[i] = finalArr[i].substr(1);
+      } // zamienia string cyfr na liczbę (czyli usuwa wszystko za wystąpieniem drugiej kropki)
+      if (!operators.includes((finalArr[i])[0])) {
+        finalArr[i] = parseFloat(finalArr[i]);
+      }
     }
 
+    if (finalArr.length < 3) return console.log('za mało stringów');
     console.log('2. makeACount finalArr ' + finalArr);
     makeANumbers(finalArr);
   }
 
   // COUNT_THIS OPRACOWANIE INPUTU
   const countThis = function () {
-    let operators = [];
     let toCount = resultField.textContent;
     if (toCount) {
       resultField.textContent = "";
       let toCountArray = [...toCount]; // zmiana na tablicę
 
       const operators = ['+', '-', '*', '/'];
-      let y = 0;
-      let x = operators.includes(toCountArray[y + 1]);
-      let z = (toCountArray[0] == '.' && operators.includes(toCountArray[1]));
-      console.log(z, x, toCountArray[1]);
 
       for (let i = 0; i < toCountArray.length; i++) {
         while (toCountArray[i] == "." && toCountArray[i + 1] == ".") { // usunięcie wielokrotnych wystąpień kropek
           toCountArray.splice(i, 1);
           console.log('czy to dziala');
+          i--;
         }
+
         // jeśli pozostała jedna kropka pomiędzy operatorami jest usunięta
-        if (toCountArray[i] == '.' && toCountArray[i - 1] == 'x') {
-          toCountArray.splice(i - 1, 2);
+        if (toCountArray[i] == '.' && operators.includes(toCountArray[i - 1]) && operators.includes(toCountArray[i + 1]) || toCountArray[i] == '.' && toCountArray.length == i) {
           console.log('USUWAM pomiędzy operatorami ' + toCountArray[i]);
+          toCountArray.splice(i, 1);
         }
+
+        // kasowanie wszystkich operatorów w ciągu poza dwoma ostatnimi
+        while (operators.includes(toCountArray[i]) && operators.includes(toCountArray[i + 1]) && operators.includes(toCountArray[i + 2])) { // usunięcie wielokrotnych wystąpień kropek
+          toCountArray.splice(i, 1);
+          console.log('obcinanie operatorów od max 2');
+          i--;
+        }
+      }
+
+      if (toCountArray[0] == '.') { // usunięcie kropki na początku
+        console.log('USUWAM pierwszy' + toCountArray[1]);
+        toCountArray.splice(0, 2);
+      }
+
+      for (let i = 0; i < toCountArray.length; i++) { // dodanie terminatorów pomiędzy ciągami operatorów i cyfr
         if (operators.includes(toCountArray[i]) && !(operators.includes(toCountArray[i + 1]))) { // dodanie x po operatorach
           toCountArray.splice(i + 1, 0, 'x');
           i++;
@@ -204,17 +214,16 @@
         }
       }
 
-      if (toCountArray[0] == '.' && toCountArray[1] == 'x') {
-        console.log('USUWAM pierwszy' + toCountArray[1]);
-        toCountArray.splice(0, 2);
-      }
-
-      if (toCountArray[toCountArray.length - 1] != 'x') { // dodanie x na końcu
+      if (toCountArray[toCountArray.length - 1] != 'x') { // dodanie x na końcu - sprawdzić czy potrzebne!
         toCountArray.push('x');
       }
-      console.log('1. toCountArray ' + toCountArray);
-      makeACount(toCountArray); // input
 
+      console.log('1. toCountArray ' + toCountArray);
+      if (toCountArray.length > 0) {
+        makeACount(toCountArray); // input
+      } else {
+        console.log('nic nie zostało!');
+      }
     } else {
       console.log('nie wprowadzono danych');
     }
